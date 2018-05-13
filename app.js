872581c9e2e393d2cafb13c06df73a5d77dddcf2
Yaraslauna Simaka <yaraslauna.simaka@gmail.com>
2018-05-13 23:16:24 +0300

    NodeJS Mentoring: Homework 2

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const config = require('./config/conf.json');
-//import config from './config/conf.json';
 const models = require('./models/models');
+const DirWatcher = require('./modules/dirwatcher').default;
+const Importer = require('./modules/importer').default;
+
+function getValue(flag) {
+    const index = process.argv.indexOf(flag);
+    return (index > -1) ? process.argv[index + 1] : null;
+}
+
+const path = __dirname + (getValue('-dir')|| ('/data/'));
+const delay = 2*1000;
+const syncImport = JSON.parse(getValue('-si')) || false;
+
+const isForFixedCount = false;
+const countFiles = 10;
+
 
 console.log(`The name of application is '${config.name}'`);
 
 const user = new models.User();
 const product = new models.Product();
+
+const dirwatcher = new DirWatcher(path, delay);
+const importer = new Importer(path, dirwatcher, syncImport);
 
--- modules/dirwatcher.js
+++ modules/dirwatcher.js
@@ -1,67 +1,98 @@
-const EventEmitter = require ('events');
+const EventEmitter = require('events');
 const fs = require('fs');
+const crypto = require('crypto');
 
-class DirWatcher  extends EventEmitter {
+class DirWatcher extends EventEmitter {
     constructor(path, delay) {
         super();
 
         this.path = path;
         this.delay = delay;
-        const oneSecond = 1000;
         this.countFiles = 0;
+        this.files = [];
 
-        // let isOdd = true;
-        // this.interval = setInterval( () => {
-        //     this.emit(isOdd ? 'tick' : 'tock');
-        //     isOdd = !isOdd;
-        // }, this.delay);
-
-        this.interval1 = setInterval( () => {
-            console.log('watch');
+        this.interval = setInterval(() => {
+            console.log('WATCH: ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''));
             this.watch();
-        }, this.delay + 1000)
+        }, this.delay)
+    }
+
+    difference(arr1, arr2) {
+        var result = arr1.filter(e => !arr2.includes(e));
+        return result;
+    }
+
+    createHash(filename) {
+        const hash = crypto.createHash('sha256');
+
+        const fileContent = fs.readFileSync(filename);
+        hash.update(fileContent);
+        const result = hash.digest('hex');
+        //console.log(`${result} of ${filename}`);
+        return result;
     }
 
     watch() {
         var dirwatcher = this;
         fs.readdir(this.path, function(err, items) {
-            if (err) {throw err;}
-            console.log(items.length);
+            var isChanged = false;
+            if (err) {
+                throw err;
+            }
+            // console.log(items.length);
 
-            if (dirwatcher.countFiles !== items.length) {
-                dirwatcher.emit('changed');
+            var wasDeleted = dirwatcher.difference(dirwatcher.files.map((e) => {
+                return e.name
+            }), items);
+            if (wasDeleted.length > 0) {
+                console.log(`Was deteled ${wasDeleted.length} files: ${wasDeleted}`);
+                dirwatcher.files = dirwatcher.files.filter(e => items.includes(e.name));
+            }
+
+            var tempList = items.filter(e => dirwatcher.files.map((el) => {
+                return el.name
+            }).includes(e));
+            var wasChanged = [];
+            tempList.forEach((e) => {
+                var hash = dirwatcher.createHash(dirwatcher.path + e);
+                var dirwatcherFile = dirwatcher.files.find((el) => (el.name == e));
+                if (dirwatcherFile.hash != hash) {
+                    wasChanged.push({
+                        name: e,
+                        hash: hash
+                    });
+
+                    dirwatcherFile.hash = hash;
+                }
+
+            });
+            if (wasChanged.length > 0) {
+                wasChanged = wasChanged.map((e) => {
+                    return e.name
+                });
+                console.log(`Was changed ${wasChanged.length} files: ${wasChanged}`);
+            }
+
+            var wasAdded = dirwatcher.difference(items, dirwatcher.files.map((e) => {
+                return e.name
+            }));
+            if (wasAdded.length > 0) {
+                console.log(`Was added ${wasAdded.length} files: ${wasAdded}`);
+                wasAdded.forEach((e) => dirwatcher.files.push({
+                    name: e,
+                    hash: dirwatcher.createHash(dirwatcher.path + e)
+                }))
+            }
+
+            isChanged = (wasDeleted.length > 0 || wasAdded.length > 0 || wasChanged.length > 0) ? true : isChanged;
+            if (isChanged) {
+                dirwatcher.emit('changed', wasAdded.concat(wasChanged));
                 dirwatcher.countFiles = items.length;
-            }
-
-            for (var i=0; i<items.length; i++) {
-                console.log(items[i]);
-            }
-
-        });
-    }
-
-    watch1() {
-        var obj = this;
-        fs.watch(this.path, {persistent: true}, function(event, filename) {
-
-            if (event == 'change'){
-                obj.emit('changed');
+            } else {
+                console.log('no any changes');
             }
         });
     }
-
-
 }
 
-const path = __dirname+'/data/';
-const delay = 2*1000;
-const dirwatcher = new DirWatcher (path, delay);
-
-//console.log('count:' + dirwatcher.read());
-dirwatcher.on('tick', () => {
-    console.log('tick');
-});
-
-dirwatcher.on('changed', () => {
-    console.log('!!!changed');
-});
+exports.default = DirWatcher;
