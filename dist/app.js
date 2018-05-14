702442312d8579c571cad9791592b30b1d7fd404
Yaraslauna_Simaka <yaraslauna.simaka@gmail.com>
2018-05-14 09:37:07 +0300

    [*] unnecessary comment lines removed

--- dist/app.js
+++ dist/app.js
@@ -10,10 +10,7 @@ var _models2 = _interopRequireDefault(_models);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//const config = require('./config/conf.json');
 console.log('The name of application is \'' + _conf2.default.name + '\'');
-//const models = require('./models/models');
-
 
 var user = new _models2.default.User();
 var product = new _models2.default.Product();
