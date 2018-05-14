192c8ca76e48436df60a4b3c6178bee2484b7e2f
Yaraslauna_Simaka <yaraslauna.simaka@gmail.com>
2018-05-14 12:44:44 +0300

    [*] minor fix in .gitignore

--- dist/models/models.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = {
-    User: require('./User.js').default,
-    Product: require('./Product.js').default
-};
